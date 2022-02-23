00baebdc0716f021e24f7411bcf57da041eadbe9
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2022-02-23 15:29:36 +0600

    removed unnecessary code

--- src/main/java/com/fractals/OES/OesApplication.java
+++ src/main/java/com/fractals/OES/OesApplication.java
@@ -1,25 +1,12 @@
 package com.fractals.OES;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OesApplication implements CommandLineRunner {
-	boolean createNewDatabase=false;
+public class OesApplication{
 	public static void main(String[] args) {
 
 		SpringApplication.run(OesApplication.class, args);
-	}
-	@Autowired
-	CreateDatabase createDatabase;
-	@Override
-	public void run(String... args)throws Exception
-	{
-		if(createNewDatabase)
-		{
-			createDatabase.create();
-		}
 	}
 }
